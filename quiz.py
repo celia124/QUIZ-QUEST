d326895a78de1899ee746ec7d286587cf5cd2961
Chase Elia <CElia@stu.kerikerihigh.ac.nz>
2021-07-07 09:47:30 +1200

    finally got my code workingish

--- quiz.py
+++ quiz.py
@@ -29,6 +29,7 @@ print ('''
 ''')
 
 num_correct = 0
+guesses = 0
 while True:
     for i, question in enumerate(questions):
         print(f"question {i + 1}")
@@ -36,23 +37,25 @@ while True:
         all_answers = []
         all_answers.append(question['correct_answer'])
         for ans in question["incorrect_answers"]:
-            all_answers.append(ans)
+            all_answers.append(unescape(ans))
         random.shuffle(all_answers)
         for num, ans in enumerate(all_answers):
             print(f"{num+1}: {ans}")
         correct_num = all_answers.index(question['correct_answer']) + 1
         user_answer = get_integer_input("Write a number between 1 and 4 to get the answer\n", "that is not one of the options", 0, 5)
         if user_answer == correct_num:
-            print("you got the right answer")
+            print("you got the right answer\n")
+            guesses += 1
             num_correct += 1
         elif user_answer != num:
-            print("you got it wrong try the next question")
-        print(correct_num)
-if num_correct == 10:
-    print("you got a perfect score good job you try again if wanted")
-if num_correct < 6 > 10:
-    print("you almost got it you need to get a bit better knowledge in gaming")
-if num_correct < 1 > 5:
-    print("you didn't get the best score")
-if num_correct == 0:
-    print("you got none correct")
+            print("you got it wrong try the next question\n")
+            guesses += 1
+    while guesses == 10:
+        if num_correct == 10:
+            print("you got a perfect score good job you try again if wanted")
+        if num_correct < 6 > 10:
+            print("you almost got it you need to get a bit better knowledge in gaming")
+        if num_correct < 1 > 6:
+            print("you didn't get the best score")
+        if num_correct == 0:
+            print("you got none correct")
