8f8e902b8f06953d3d25e619a3181d3d667138dc
Chase Elia <CElia@stu.kerikerihigh.ac.nz>
2021-08-04 09:55:21 +1200

    i added a timer to my code

--- quiz.py
+++ quiz.py
@@ -2,6 +2,7 @@ import requests
 import random
 from html import unescape
 from random import choice
+import time
 
 def get_integer_input(message, error, low, high):
     ''' '''
@@ -27,6 +28,7 @@ def get_questions(difficulty):
 
 
 while True:
+    #this is to get the difficulty and stuff
     response = requests.get('https://opentdb.com/api.php?amount=10&category=15&type=multiple')
 
     questions4 = response.json()['results']
@@ -53,6 +55,7 @@ while True:
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     ''')
+    # This is to get which gamemode they want
     gamemode = get_integer_input("What gamemode would you like \n Type 1 for easy \n Type 2 for medium \n Type 3 for hard \n Type 4 for a mix of all \n" , "That's not one of the options \n" , 0,5)
     if gamemode == 1:
         questions = questions1
@@ -62,6 +65,11 @@ while True:
         questions = questions3
     if gamemode == 4:
         questions = questions4
+    gamemode1 = get_integer_input("Do you want a normal game, timed or speed rounds \n Type 1 for normal \n Type 2 for timed \n Type 3 speed rounds\n", "That is not an option", 0,4)
+    if gamemode1 == 2:
+        start = time.time()
+    if gamemode1 == 3:
+        break
     num_correct = 0
     guesses = 0
     #this bit is a little confusing for me but its how the questions get in order and which one is correct.
@@ -76,7 +84,6 @@ while True:
         for num, ans in enumerate(all_answers):
             print(f"{num+1}: {unescape(ans)}")
         correct_num = all_answers.index(question['correct_answer']) + 1
-        print(correct_num)
         user_answer = get_integer_input("Write a number between 1 and 4 to get the answer ", "that is not one of the options", 0, 5)
         #this is the area that says if you got it wrong or right it's pretty simple but does the job
         if user_answer == correct_num:
@@ -88,83 +95,16 @@ while True:
             print("you got it wrong try the next question\n")
             print(f"the right answer is {correct_num}\n")
             guesses += 1
-    # if gamemode == 2:
-    #     for i, question in enumerate(questions2):
-    #         print(f"Question {i + 1}")
-    #         print(unescape(question['question']))
-    #         all_answers = []
-    #         all_answers.append(question['correct_answer'])
-    #         for ans in question["incorrect_answers"]:
-    #             all_answers.append(unescape(ans))
-    #         random.shuffle(all_answers)
-    #         for num, ans in enumerate(all_answers):
-    #             print(f"{num+1}: {unescape(ans)}")
-    #         correct_num = all_answers.index(question['correct_answer']) + 1
-    #         user_answer = get_integer_input("Write a number between 1 and 4 to get the answer ", "that is not one of the options", 0, 5)
-    #         #this is the area that says if you got it wrong or right it's pretty simple but does the job
-    #         if user_answer == correct_num:
-    #             print("you got the right answer")
-    #             print(f"the right answer is {correct_num}\n")
-    #             guesses += 1
-    #             num_correct += 1
-    #         elif user_answer != correct_num:
-    #             print("you got it wrong try the next question\n")
-    #             print(f"the right answer is {correct_num}\n")
-    #             guesses += 1
-    # if gamemode == 3:
-    #     for i, question in enumerate(questions3):
-    #         print(f"Question {i + 1}")
-    #         print(unescape(question['question']))
-    #         all_answers = []
-    #         all_answers.append(question['correct_answer'])
-    #         for ans in question["incorrect_answers"]:
-    #             all_answers.append(unescape(ans))
-    #         random.shuffle(all_answers)
-    #         for num, ans in enumerate(all_answers):
-    #             print(f"{num+1}: {unescape(ans)}")
-    #         correct_num = all_answers.index(question['correct_answer']) + 1
-    #         user_answer = get_integer_input("Write a number between 1 and 4 to get the answer ", "that is not one of the options", 0, 5)
-    #         #this is the area that says if you got it wrong or right it's pretty simple but does the job
-    #         if user_answer == correct_num:
-    #             print("you got the right answer")
-    #             print(f"the right answer is {correct_num}\n")
-    #             guesses += 1
-    #             num_correct += 1
-    #         elif user_answer != correct_num:
-    #             print("you got it wrong try the next question\n")
-    #             print(f"the right answer is {correct_num}\n")
-    #             guesses += 1
-    # if gamemode == 4:
-    #     for i, question in enumerate(questions):
-    #         print(f"Question {i + 1}")
-    #         print(unescape(question['question']))
-    #         all_answers = []
-    #         all_answers.append(question['correct_answer'])
-    #         for ans in question["incorrect_answers"]:
-    #             all_answers.append(unescape(ans))
-    #         random.shuffle(all_answers)
-    #         for num, ans in enumerate(all_answers):
-    #             print(f"{num+1}: {unescape(ans)}")
-    #         correct_num = all_answers.index(question['correct_answer']) + 1
-    #         user_answer = get_integer_input("Write a number between 1 and 4 to get the answer ", "that is not one of the options", 0, 5)
-    #         #this is the area that says if you got it wrong or right it's pretty simple but does the job
-    #         if user_answer == correct_num:
-    #             print("you got the right answer")
-    #             print(f"the right answer is {correct_num}\n")
-    #             guesses += 1
-    #             num_correct += 1
-    #         elif user_answer != correct_num:
-    #             print("you got it wrong try the next question\n")
-    #             print(f"the right answer is {correct_num}\n")
-    #             guesses += 1
-    # i am trying to make a finishing question thing that tells you your score once you're done
+    if gamemode1 == 2:
+        end = time.time()
     if guesses == 10:
         percent = num_correct * guesses
         print(f"~~~~~~~~~~~~~~~~You finished the quiz good job~~~~~~~~~~~~~~~~~~~~~ \nYou anwsered {percent}% of questions right")
-    elif percent == 100:
-        print("You got a perfect score")
-    elif percent < 50 > 100:
-        print ("You got a pass good job")
-    else:
-        print("You failed")
+        print(f"You finished in {round(end-start,1)} seconds")
+        if percent == 100:
+            print("You got a perfect score")
+        elif percent < 50 > 100:
+            print ("You got a pass good job")
+        else:
+            print("You failed")
     break
